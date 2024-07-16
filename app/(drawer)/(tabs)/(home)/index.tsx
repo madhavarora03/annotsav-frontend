e55dbd947b5f6ae057c354465abcf00803000c7d
madhavarora03 <eragon.cube@outlook.com>
2024-07-16 11:01:35 +0530

    Showing Crop status and moisture level bar on home page

--- app/(drawer)/(tabs)/(home)/index.tsx
+++ app/(drawer)/(tabs)/(home)/index.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Card,
   H2,
+  H3,
   Paragraph,
   ScrollView,
   Spinner,
@@ -19,10 +20,10 @@ export default function Page() {
   const [isFetching, setIsFetching] = useState(false);
   const [data, setData] = useState<{
     daily: {
-      time: any;
-      temperature2mMax: any;
-      precipitationProbabilityMax: any;
-      windSpeed10mMax: any;
+      time: string[];
+      temperature2mMax: number[];
+      precipitationProbabilityMax: number[];
+      windSpeed10mMax: number[];
     };
   } | null>(null);
   const theme = useTheme();
@@ -53,70 +54,86 @@ export default function Page() {
 
   const loading = isLoading || isFetching;
   return (
-    <Card elevate size="$4" bordered height={300} scale={0.95}>
-      <Card.Header padded>
-        <XStack justifyContent="space-between" alignItems="center">
-          <H2>Weather Update</H2>
-          {loading ? (
-            <Spinner size="large" color="$green10" />
-          ) : (
-            <Button borderRadius="$12" onPress={handleRefresh}>
-              <Feather name="refresh-cw" size={24} color="black" />
-            </Button>
-          )}
-        </XStack>
+    <YStack paddingVertical="$4">
+      <Card elevate size="$4" bordered scale={0.95}>
+        <Card.Header padded>
+          <H2>Crop Status</H2>
+          <YStack width="100%" paddingTop="$3" gap="$2">
+            <H3 height={30} paddingLeft="$1" opacity={0.5}>
+              Moisture Level: 60%
+            </H3>
+            <ProgressBar />
+          </YStack>
+        </Card.Header>
+      </Card>
+      <Card elevate size="$4" bordered scale={0.95}>
+        <Card.Header padded>
+          <XStack justifyContent="space-between" alignItems="center">
+            <H2>Weather Update</H2>
+            {loading ? (
+              <Spinner size="large" color="$green10" marginRight="$3" />
+            ) : (
+              <Button borderRadius="$12" onPress={handleRefresh}>
+                <Feather name="refresh-cw" size={24} color="black" />
+              </Button>
+            )}
+          </XStack>
 
-        <Paragraph color={theme.green8.get()}>3 Day Forecast</Paragraph>
-      </Card.Header>
-      <ScrollView
-        padding="$4"
-        borderRadius="$4"
-        alignContent="center"
-        horizontal
-        contentContainerStyle={{ gap: 14, paddingLeft: 7 }}
-        showsHorizontalScrollIndicator={false}
-      >
-        {loading && <Text>Loading...</Text>}
-        {error && <Text>{error.message}</Text>}
-        {data &&
-          data.daily.time.map((time: any, index: any) => (
-            <YStack key={index} gap="$1" alignItems="center">
-              <Text
-                fontSize="$5"
-                textAlign="center"
-                color={theme.green10.get()}
-                paddingBottom="$2"
-              >
-                {time.toDateString()}
-              </Text>
-              <XStack alignItems="center" gap="$1.5">
-                <Feather name="thermometer" size={20} color="black" />
-                <Text>{data.daily.temperature2mMax[index].toFixed(2)}°C</Text>
-              </XStack>
-              <XStack alignItems="center" gap="$1.5">
-                <Feather name="wind" size={20} color="black" />
-                <Text>{data.daily.windSpeed10mMax[index].toFixed(2)} km/h</Text>
-              </XStack>
-              <XStack alignItems="center" gap="$1.5">
-                <Feather name="droplet" size={20} color="black" />
-                <Text>{data.daily.precipitationProbabilityMax[index]}%</Text>
-              </XStack>
-            </YStack>
-          ))}
-      </ScrollView>
-      <Card.Footer padded>
-        <YStack width="100%">
-          <Paragraph
-            height={30}
-            paddingLeft="$1"
-            opacity={0.5}
-            color={theme.accentColor.get()}
-          >
-            Moisture Level: 60%
-          </Paragraph>
-          <ProgressBar />
-        </YStack>
-      </Card.Footer>
-    </Card>
+          <Paragraph color={theme.green8.get()}>3 Day Forecast</Paragraph>
+        </Card.Header>
+        <ScrollView
+          padding="$4"
+          borderRadius="$4"
+          alignContent="center"
+          horizontal
+          contentContainerStyle={{ gap: 14 }}
+          showsHorizontalScrollIndicator={false}
+        >
+          {loading && (
+            <XStack width="100%" alignItems="center">
+              <Paragraph width="100%" paddingLeft="$1" opacity={0.5}>
+                Loading...
+              </Paragraph>
+            </XStack>
+          )}
+          {error && (
+            <XStack width="100%" alignItems="center">
+              <Paragraph width="100%" paddingLeft="$1" opacity={0.5}>
+                {error.message}
+              </Paragraph>
+            </XStack>
+          )}
+          {data &&
+            !loading &&
+            data.daily.time.map((time: any, index: any) => (
+              <YStack key={index} gap="$1" alignItems="center">
+                <Text
+                  fontSize="$5"
+                  textAlign="center"
+                  color={theme.green10.get()}
+                  paddingBottom="$2"
+                >
+                  {time.toDateString()}
+                </Text>
+                <XStack alignItems="center" gap="$1.5">
+                  <Feather name="thermometer" size={20} color="black" />
+                  <Text>{data.daily.temperature2mMax[index].toFixed(2)}°C</Text>
+                </XStack>
+                <XStack alignItems="center" gap="$1.5">
+                  <Feather name="wind" size={20} color="black" />
+                  <Text>
+                    {data.daily.windSpeed10mMax[index].toFixed(2)} km/h
+                  </Text>
+                </XStack>
+                <XStack alignItems="center" gap="$1.5">
+                  <Feather name="droplet" size={20} color="black" />
+                  <Text>{data.daily.precipitationProbabilityMax[index]}%</Text>
+                </XStack>
+              </YStack>
+            ))}
+        </ScrollView>
+        <Card.Footer padded></Card.Footer>
+      </Card>
+    </YStack>
   );
 }
