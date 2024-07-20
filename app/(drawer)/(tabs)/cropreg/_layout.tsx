66e31bb7e3dbb91b723e31a23637dfae4ba249c5
CholleKulche <jyotin.singh.ug23@nsut.ac.in>
2024-07-20 21:35:06 +0530

    Added crop reg

---
+++ app/(drawer)/(tabs)/cropreg/_layout.tsx
@@ -0,0 +1,41 @@
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import { Stack } from "expo-router";
+import { Image, useTheme } from "tamagui";
+
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
+const Layout = () => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Image
+            source={{
+              uri: "../../../assets/logo.png",
+              height: 75,
+              width: 75,
+            }}
+          />
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "My Device",
+          headerTintColor: "white",
+          headerLeft: () => <DrawerToggleButton tintColor="#fff" />,
+          headerStyle: {
+            backgroundColor: theme.green10.get(),
+          },
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
